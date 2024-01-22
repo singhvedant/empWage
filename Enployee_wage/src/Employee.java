51ab334d09bb1268ea36c1e48c07e27296cd13bb
Vedant Singh <singhvedant1701@gmail.com>
2024-01-22 17:38:45 +0530

    Use case 2

--- Enployee_wage/src/Employee.java
+++ Enployee_wage/src/Employee.java
@@ -2,8 +2,14 @@ public class Employee {
     String name;
     Integer id;
 
-    Employee(String n, Integer i){
+    Integer wagePerHour;
+
+    Integer workHour = 0;
+    boolean attendence;
+
+    Employee(String n, Integer i,Integer w){
         this.name = n;
         this.id = i;
+        this.wagePerHour = w;
     }
 }
--- Enployee_wage/src/Main.java
+++ Enployee_wage/src/Main.java
@@ -1,21 +1,29 @@
-
 public class Main {
     public static void main(String[] args) {
-            Employee e1 = new Employee("Bharat", 1);
-        int check = (int) ((Math.random() * 10)) % 2;
+        Employee e1 = new Employee("Bharat",1,200);
+        int check = (int)((Math.random()*10))%2;
 
-        if (check == 0) {
-            e1.attendence = 0;
-        } else {
-            e1.attendence = 1;
+        if (check == 0){
+            e1.attendence = false;
+        }else {
+            e1.attendence = true;
         }
 
-        if (e1.attendence == 0) {
-            e1.attendence = 0;
-        }else {
-            e1.attendence = 1;
+        if(e1.attendence){
             System.out.println("Employee is present");
             e1.workHour = 8;
+        }else {
+            System.out.println("Employee is absent");
         }
+
+        //it is a Not static method
+        Main m1 = new Main();
+        System.out.println(m1.calculateSalary(e1));
+
+
+    }
+    double calculateSalary(Employee e){
+        return e.workHour*e.wagePerHour;
     }
 }
+
